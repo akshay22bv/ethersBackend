8f439e506bc81d369d06e57e68c6fb1190dc9b9e
akshay22bv <akshay22bv@gmail.com>
2024-06-07 11:33:55 +0530

    config file changes

--- config/sequelize-config.js
+++ config/sequelize-config.js
@@ -2,36 +2,29 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = {
-  local: {
+  // local: {
+  //   username: "postgres",
+  //   password: "Avb@1221",
+  //   database: "exchange",
+  //   host: "localhost",
+  //   dialect: "postgres",
+  //   port: 5432,
+  // },
+
+  development: {
     username: "postgres",
     password: "Avb@1221",
     database: "exchange",
     host: "localhost",
     dialect: "postgres",
+    timezone: "utc",
+    port: 5432,
+    ssl: true,
+    pool: {
+      max: 100,
+      min: 0,
+      acquire: 60000,
+      idle: 20000,
+    },
   },
-  //   development: {
-  //     username: "redspace",
-  //     password: "Admin@123",
-  //     database: "exchange_db",
-  //     host: "dev-exchange-crm-db.postgres.database.azure.com",
-  //     dialect: "postgres",
-  //     timezone: "utc",
-  //     dialectOptions: {
-  //       useUTC: false,
-  //       timezone: "utc",
-  //       ssl: {
-  //         ca: fs
-  //           .readFileSync(path.resolve(__dirname, "./exchangedbca.pem"))
-  //           .toString(),
-  //       },
-  //     },
-  //     port: 5432,
-  //     ssl: true,
-  //     pool: {
-  //       max: 5,
-  //       min: 0,
-  //       acquire: 60000,
-  //       idle: 20000,
-  //     },
-  //   },
 };
--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const process = require("process");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "local";
+const env = process.env.NODE_ENV || "development";
 const config = require(__basedir + "/config/sequelize-config")[env];
 const db = {};
 
@@ -20,6 +20,17 @@ if (config.use_env_variable) {
     config
   );
 }
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log(
+      "Connection to the database has been established successfully."
+    );
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
 fs.readdirSync(__dirname)
   .filter((file) => {
